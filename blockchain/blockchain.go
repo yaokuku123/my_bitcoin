0fb7aa6b413c6a7dbc9928d0ca92d751a6fd496f
yaokuku123 <yaoqijun@outlook.com>
2022-10-21 14:21:10 +0800

    向区块链中添加新区块

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -12,3 +12,13 @@ func NewBlockChain() *BlockChain {
 	}
 	return &blockChain
 }
+
+// AddBlock 添加新区块到区块链
+func (bc *BlockChain) AddBlock(data string) {
+	// 1.获取前区块的 hash
+	prevHash := bc.Blocks[len(bc.Blocks)-1].Hash
+	// 2.创建新区块
+	block := NewBlock(data, prevHash)
+	// 3.将新区块添加到区块链中
+	bc.Blocks = append(bc.Blocks, block)
+}
--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	bc := blockchain.NewBlockChain()
-	for _, block := range bc.Blocks {
-		fmt.Printf("prevHash:%v\n", block.PrevHash)
-		fmt.Printf("hash:%v\n", block.Hash)
+	bc.AddBlock("transfer 50 coin")
+	bc.AddBlock("transfer 20 coin")
+	for i, block := range bc.Blocks {
+		fmt.Printf("----------%d-----------\n", i)
+		fmt.Printf("prevHash:%x\n", block.PrevHash)
+		fmt.Printf("hash:%x\n", block.Hash)
 		fmt.Printf("data:%s\n", block.Data)
 	}
 }
